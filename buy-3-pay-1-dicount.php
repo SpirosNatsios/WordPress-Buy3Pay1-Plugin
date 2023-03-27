ceb865ebdf1597d22304d83ba3edc748b9a9e06a
Spiros Natsios <spi.natsios@gmail.com>
2023-03-27 14:51:52 +0300

    Fixed a bug where if no category was selected the discount was not applied at all

--- buy-3-pay-1-dicount.php
+++ buy-3-pay-1-dicount.php
@@ -1,33 +1,25 @@
 <?php
 
 add_action('woocommerce_cart_calculate_fees', 'discount_for_every_3_items');
-
+    
 function discount_for_every_3_items($cart)
 {
-
     // Define the excluded categories by their term IDs
     $excluded_categories = $excluded_categories = get_option('b3p1_excluded_categories', array());
 
     // Calculate the number of items in the cart
-    $cart_items_count = 0;
-    $cart_items = $cart->get_cart();
-    foreach ($cart_items as $cart_item) {
-        // Skip items that belong to excluded categories
-        if (has_term($excluded_categories, 'product_cat', $cart_item['product_id'])) {
-            continue;
-        }
-        $cart_items_count += $cart_item['quantity'];
-    }
+    $cart_items_count = $cart->get_cart_contents_count();
+
     // If there are less than 3 items in the cart, no discount is applied
     if ($cart_items_count < 3) {
         return;
     }
 
-    // Get the price of the items in the cart 
+    // Get the price of the items in the cart, excluding any items that belong to the excluded categories
     $cart_items = $cart->get_cart();
     $total_price = 0;
     foreach ($cart_items as $cart_item) {
-        if (has_term($excluded_categories, 'product_cat', $cart_item['product_id'])) {
+        if (!empty($excluded_categories) && has_term($excluded_categories, 'product_cat', $cart_item['product_id'])) {
             continue;
         }
         $total_price += $cart_item['data']->get_price_excluding_tax() * $cart_item['quantity'];
@@ -36,7 +28,6 @@ function discount_for_every_3_items($cart)
     // Calculate the discount amount as the price of two items for every set of three items
     $number_of_sets_of_three = floor($cart_items_count / 3);
     $discount_amount = $number_of_sets_of_three * 2 * ($total_price / $cart_items_count);
-
 
     //Apply the discount to the cart
     $cart->add_fee(__('Discount', 'woocommerce'), -$discount_amount);
--- buy-3-pay-1-settings.php
+++ buy-3-pay-1-settings.php
@@ -1,18 +1,17 @@
-
-<?php 
+<?php
 add_action('admin_menu', 'b3p1_add_options_page');
 add_action('admin_init', 'b3p1_register_settings');
 
 function b3p1_add_options_page()
 {
     add_submenu_page(
-    'woocommerce',
-    'Buy 3 Pay 1 Options',
-    'Buy 3 Pay 1',
-    'manage_options',
-    'buy-3-pay-1',
-    'b3p1_options_page'
-);
+        'woocommerce',
+        'Buy 3 Pay 1 Options',
+        'Buy 3 Pay 1',
+        'manage_options',
+        'buy-3-pay-1',
+        'b3p1_options_page'
+    );
 }
 
 function b3p1_register_settings()
@@ -30,24 +29,32 @@ function b3p1_options_page()
     }
 
     $excluded_categories = get_option('b3p1_excluded_categories', array());
-    $categories = get_terms(array(
-        'taxonomy' => 'product_cat',
-        'hide_empty' => false,
-    ));
-?>
+    $categories = get_terms(
+        array(
+            'taxonomy' => 'product_cat',
+            'hide_empty' => false,
+        )
+    );
+    ?>
     <div class="wrap">
-        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <h1>
+            <?php echo esc_html(get_admin_page_title()); ?>
+        </h1>
         <form action="options.php" method="post">
             <?php settings_fields('b3p1_options_group'); ?>
             <?php do_settings_sections('b3p1_options_group'); ?>
             <table class="form-table">
                 <tbody>
                     <tr valign="top">
-                        <th scope="row"><?php _e('Excluded Categories', 'b3p1'); ?></th>
+                        <th scope="row">
+                            <?php _e('Excluded Categories', 'b3p1'); ?>
+                        </th>
                         <td>
-                            <?php foreach ($categories as $category) : ?>
+                            <?php foreach ($categories as $category): ?>
                                 <label>
-                                    <input type="checkbox" name="b3p1_excluded_categories[]" value="<?php echo esc_attr($category->term_id); ?>" <?php checked(in_array($category->term_id, $excluded_categories), true); ?>>
+                                    <input type="checkbox" name="b3p1_excluded_categories[]"
+                                        value="<?php echo esc_attr($category->term_id); ?>" <?php if (is_array($excluded_categories) && in_array($category->term_id, $excluded_categories))
+                                               echo 'checked="checked"'; ?>>
                                     <?php echo esc_html($category->name); ?>
                                 </label><br>
                             <?php endforeach; ?>
@@ -59,6 +66,6 @@ function b3p1_options_page()
         </form>
 
     </div>
-<?php
+    <?php
 }
 ?>
